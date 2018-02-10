248d8c59e57d33dd5065ad1460605349ced65b24
AppyFizz <21978236+appyfizz@users.noreply.github.com>
2018-02-10 04:02:03 -0500

    fixed

--- app/src/main/java/com/example/shreyan/tartanhacks/MainActivity.java
+++ app/src/main/java/com/example/shreyan/tartanhacks/MainActivity.java
@@ -30,7 +30,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package com.microsoft.CognitiveServicesExample;
+package com.example.shreyan.tartanhacks;
 
 import android.app.Activity;
 import android.app.AlertDialog;
